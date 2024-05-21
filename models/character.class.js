3216ff3c8c45911d8c55542ddca0e966e40d403b
Oliver Drexler <oliverdrexler@web.de>
2024-05-21 12:31:34 +0200

    added constructor()

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,9 @@
 class Character extends MovableObject {
 
-    
+    constructor() {
+        super().loadImage('../img/2_character_pepe/2_walk/W-21.png'); // mit super wird die Funtion der übergeordneten Klasse (=MoveableObject) aufgerufen
+    }
+
     jump() {
 
     }
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,3 +1,6 @@
 class Chicken extends MovableObject {
    
+    constructor() {
+        super().loadImage('../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png'); // mit super wird die Funtion der übergeordneten Klasse (=MoveableObject) aufgerufen
+    }
 }
