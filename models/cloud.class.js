86058e2535933ab27b1d6915856b49d340079c7d
Oliver Drexler <oliverdrexler@web.de>
2024-05-21 17:07:10 +0200

    added background class

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,12 +1,13 @@
 class Cloud extends MovableObject {
-y = 20; // bei Wolken dynamisch, daher hier definiert
+// Werte bei Wolken nicht dynamisch, daher hier definiert
+y = 20; 
+height = 250;
+width = 500;
 
 constructor() {
     super().loadImage('../img/5_background/layers/4_clouds/1.png');
 
     this.x = Math.random() * 700; 
-    this.width = 500;
-    this.height = 300;
 }
 
 }
