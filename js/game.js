a1e30cdf762f25d134acc347e81bfd03c895aff1
Oliver Drexler <oliverdrexler@web.de>
2024-05-22 14:56:57 +0200

    added eventListener for keydown events

--- js/game.js
+++ js/game.js
@@ -9,3 +9,17 @@ function init() {
 
     console.log('my character is', world.character)
 }
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowLeft') {
+        console.log('Left was pressed');
+    } else if (event.key === 'ArrowRight') {
+        console.log('Right was pressed');
+    } else if (event.key === 'ArrowUp') {
+        console.log('Up was pressed');
+    } else if (event.key === 'ArrowDown') {
+        console.log('Down was pressed');
+    } else if (event.key === ' ') {
+        console.log('Space was pressed');
+    }
+});
