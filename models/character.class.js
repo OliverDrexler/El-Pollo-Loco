c56e14024437a0f109bd6472d672e3c52dc3ff09
Oliver Drexler <oliverdrexler@web.de>
2024-05-22 13:47:24 +0200

    added animation to character

--- models/character.class.js
+++ models/character.class.js
@@ -1,18 +1,30 @@
 class Character extends MovableObject {
     height = 300;
     y = 130;
+    IMAGES_WALKING = [
+        '../img/2_character_pepe/2_walk/W-21.png',
+        '../img/2_character_pepe/2_walk/W-22.png',
+        '../img/2_character_pepe/2_walk/W-23.png',
+        '../img/2_character_pepe/2_walk/W-24.png',
+        '../img/2_character_pepe/2_walk/W-25.png',
+        '../img/2_character_pepe/2_walk/W-26.png'
+    ];
+    currentImage = 0;
 
     constructor() {
         // mit super wird die Funtion der übergeordneten Klasse (=MoveableObject) aufgerufen
         super().loadImage('../img/2_character_pepe/2_walk/W-21.png');
-        this.loadImages([
-            '../img/2_character_pepe/2_walk/W-21.png',
-            '../img/2_character_pepe/2_walk/W-22.png',
-            '../img/2_character_pepe/2_walk/W-23.png',
-            '../img/2_character_pepe/2_walk/W-24.png',
-            '../img/2_character_pepe/2_walk/W-25.png',
-            '../img/2_character_pepe/2_walk/W-26.png'
-        ]);
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            let path = this.IMAGES_WALKING[this.currentImage];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 500);
     }
 
     jump() {
