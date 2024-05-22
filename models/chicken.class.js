9b19a163c1e50e51829bf13a0e78217cfde88c33
Oliver Drexler <oliverdrexler@web.de>
2024-05-22 14:17:53 +0200

    Update chicken.class.js

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,12 +2,33 @@ class Chicken extends MovableObject {
     y = 345;
     width = 80;
     height = 80;
+    IMAGES_WALKING = [
+        '../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        '../img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        '../img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+    ];
+    currentImage = 0;
     
     constructor() {
         // mit super wird die Funtion der übergeordneten Klasse (=MoveableObject) aufgerufen
         super().loadImage('../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
+        this.loadImages(this.IMAGES_WALKING);
+
         // // Zahl zwischen 200 & 700
         this.x = 200 + Math.random() * 500;
+
+        this.animate();
     }
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 1 / 6 -> 0 Rest 1 usw...
+            // i = 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, ....
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 150);
+    }
+
 }
