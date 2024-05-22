d2cf14b91a60e8ba76135bedebbc06b182addd1e
Oliver Drexler <oliverdrexler@web.de>
2024-05-22 09:06:54 +0200

    added background layers

--- models/world.class.js
+++ models/world.class.js
@@ -9,7 +9,9 @@ class World {
         new Cloud(),
     ];
     backgroundObjects = [
-        new BackgroundObject('../img/5_background/layers/3_third_layer/1.png', 0, 80)
+        new BackgroundObject('../img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('../img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('../img/5_background/layers/1_first_layer/1.png', 0)
     ];
     canvas; // neue Variable für draw function
     ctx;
@@ -24,10 +26,10 @@ class World {
         // löscht das canvas, um es direk danach neu zu zeichnen (für bewegte Bilder)
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); 
 
-        this.addToMap(this.character);   
-        this.addObjectsToMap(this.enemies);  
         this.addObjectsToMap(this.clouds);
         this.addObjectsToMap(this.backgroundObjects);
+        this.addObjectsToMap(this.enemies);  
+        this.addToMap(this.character);   
 
         // this. kann in requestAnimationFrame nicht verwendet werden, daher neue Variable self, mit der draw() immer wieder aufgerufen wird
         let self = this; 
