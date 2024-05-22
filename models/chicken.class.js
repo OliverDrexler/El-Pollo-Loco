c1cb1aaaca35f621bb83c808af2dff6b30825840
Oliver Drexler <oliverdrexler@web.de>
2024-05-22 09:17:22 +0200

    fixed character & enemy positioning

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,8 +1,9 @@
 class Chicken extends MovableObject {
-   
+    y = 290;
+    
     constructor() {
         // mit super wird die Funtion der übergeordneten Klasse (=MoveableObject) aufgerufen
-        super().loadImage('../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png'); 
+        super().loadImage('../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
         // // Zahl zwischen 200 & 700
         this.x = 200 + Math.random() * 500;
