5683a7857f4bb78f01271753b493bd3108008c63
Oliver Drexler <oliverdrexler@web.de>
2024-05-23 11:06:14 +0200

    added background and created level.class.js

--- models/world.class.js
+++ models/world.class.js
@@ -9,6 +9,10 @@ class World {
         new Cloud(),
     ];
     backgroundObjects = [
+        new BackgroundObject('../img/5_background/layers/air.png', -719),
+        new BackgroundObject('../img/5_background/layers/3_third_layer/2.png', -719),
+        new BackgroundObject('../img/5_background/layers/2_second_layer/2.png', -719),
+        new BackgroundObject('../img/5_background/layers/1_first_layer/2.png', -719),
         new BackgroundObject('../img/5_background/layers/air.png', 0),
         new BackgroundObject('../img/5_background/layers/3_third_layer/1.png', 0),
         new BackgroundObject('../img/5_background/layers/2_second_layer/1.png', 0),
@@ -16,8 +20,15 @@ class World {
         new BackgroundObject('../img/5_background/layers/air.png', 719),
         new BackgroundObject('../img/5_background/layers/3_third_layer/2.png', 719),
         new BackgroundObject('../img/5_background/layers/2_second_layer/2.png', 719),
-        new BackgroundObject('../img/5_background/layers/1_first_layer/2.png', 719)
-
+        new BackgroundObject('../img/5_background/layers/1_first_layer/2.png', 719),
+        new BackgroundObject('../img/5_background/layers/air.png', 719*2),
+        new BackgroundObject('../img/5_background/layers/3_third_layer/1.png', 719*2),
+        new BackgroundObject('../img/5_background/layers/2_second_layer/1.png', 719*2),
+        new BackgroundObject('../img/5_background/layers/1_first_layer/1.png', 719*2),
+        new BackgroundObject('../img/5_background/layers/air.png', 719*3),
+        new BackgroundObject('../img/5_background/layers/3_third_layer/2.png', 719*3),
+        new BackgroundObject('../img/5_background/layers/2_second_layer/2.png', 719*3),
+        new BackgroundObject('../img/5_background/layers/1_first_layer/2.png', 719*3)
     ];
     canvas; // neue Variable für draw function
     ctx;
