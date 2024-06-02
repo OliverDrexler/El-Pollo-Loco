c6e1a8f8da43e90dc329a8764be1d7d48bb87d35
Oliver Drexler <oliverdrexler@web.de>
2024-06-02 11:44:10 +0200

    added endboss images

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -1,5 +1,21 @@
 class Endboss extends MovableObject {
 
+    IMAGES_WALKING = [
+        '../img/4_enemie_boss_chicken/2_alert/G5.png',
+        '../img/4_enemie_boss_chicken/2_alert/G6.png',
+        '../img/4_enemie_boss_chicken/2_alert/G7.png',
+        '../img/4_enemie_boss_chicken/2_alert/G8.png',
+        '../img/4_enemie_boss_chicken/2_alert/G9.png',
+        '../img/4_enemie_boss_chicken/2_alert/G10.png',
+        '../img/4_enemie_boss_chicken/2_alert/G11.png',
+        '../img/4_enemie_boss_chicken/2_alert/G12.png'
+    ];
 
-    
+    constructor() {
+        super().loadImage('../img/4_enemie_boss_chicken/2_alert/G5.png');
+
+        this.loadImages(this.IMAGES_WALKING);
+    }
+
+
 }
