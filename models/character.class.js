3589d8873ec28de02adb074bbbf32a7b128dd165
Oliver Drexler <oliverdrexler@web.de>
2024-06-02 12:05:10 +0200

    cleaned code with adding playAnimation() function

--- models/character.class.js
+++ models/character.class.js
@@ -54,11 +54,7 @@ class Character extends MovableObject {
         // walk animation
         setInterval(() => {
             if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT ) {
-                let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 1 / 6 -> 0 Rest 1 usw...
-                // i = 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, ....
-                let path = this.IMAGES_WALKING[i];
-                this.img = this.imageCache[path];
-                this.currentImage++;
+                this.playAnimation(this.IMAGES_WALKING);
             }
         }, 80);
     }
--- models/endboss.class.js
+++ models/endboss.class.js
@@ -14,6 +14,7 @@ class Endboss extends MovableObject {
     constructor() {
         super().loadImage('../img/4_enemie_boss_chicken/2_alert/G5.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.x = 600;
     }
 
 
