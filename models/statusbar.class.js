16db6da47b3b04ee212115ee8d685cf26d040456
Oliver Drexler <oliverdrexler@web.de>
2024-06-04 12:41:48 +0200

    Update statusbar.class.js

--- models/statusbar.class.js
+++ models/statusbar.class.js
@@ -8,4 +8,35 @@ class Statusbar {
         '../img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png',
         '../img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png'
     ];
+
+    percentage = 100;
+
+
+    constructor() {
+        this.loadImages(this.IMAGES_STATUSBAR);
+    }
+
+
+    setPercentage(percentage) {
+        this.percentage = percentage;
+
+        
+    }
+
+    resolveImageIndex() {
+        if (this.percentage == 100) {
+            return 5;
+        } else if (this.percentage > 80) {
+            return 4;
+        } else if (this.percentage > 60) {
+            return 3;
+        } else if (this.percentage > 40) {
+            return 2;
+        } else if (this.percentage > 20) {
+            return 1;
+        } else /*if (this.percentage == 0)*/ {
+            return 0;
+        }
+    }
+
 }
