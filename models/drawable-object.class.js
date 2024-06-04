12fef5583d1290d08e7767209e0a341d7c559ac7
Oliver Drexler <oliverdrexler@web.de>
2024-06-04 12:23:56 +0200

    refactored code

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -1,5 +1,5 @@
 class DrawableObject {
-    
+
     x = 120;
     y = 180;
     height = 250;
@@ -7,4 +7,37 @@ class DrawableObject {
     img;
     imageCache = {};
     currentImage = 0;
+
+
+    /**
+     * This method loads an image from the specified path.
+     * @param {string} path - The path to the image file.
+     */
+    loadImage(path) {
+        this.img = new Image(); // Image is predefined
+        this.img.src = path;
+    }
+
+
+    /**
+    * This method draws the object on the canvas.
+    * @param {CanvasRenderingContext2D} ctx - The drawing context of the canvas.
+    */
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+
+    /**
+     * This method loads multiple images and caches them.
+     * @param {string[]} arr - An array of image paths.
+     */
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+    }
+
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -30,26 +30,7 @@ class MovableObject extends DrawableObject {
     isAboveGround() {
         return this.y < 120;
     }
-
-
-    /**
-     * This method loads an image from the specified path.
-     * @param {string} path - The path to the image file.
-     */
-    loadImage(path) {
-        this.img = new Image(); // Image is predefined
-        this.img.src = path;
-    }
-
-
-    /**
-     * This method draws the object on the canvas.
-     * @param {CanvasRenderingContext2D} ctx - The drawing context of the canvas.
-     */
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
-
+    
 
     /**
      * This method draws a frame around the objects on the canvas 
@@ -129,19 +110,6 @@ class MovableObject extends DrawableObject {
     }*/
 
     ////////////////////////////////////////////////////////////
-
-
-    /**
-     * This method loads multiple images and caches them.
-     * @param {string[]} arr - An array of image paths.
-     */
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
 
 
     /**
