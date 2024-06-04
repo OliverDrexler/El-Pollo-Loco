6eadc816e6ffa191ff585e5d0d2167b0901c0528
Oliver Drexler <oliverdrexler@web.de>
2024-06-04 12:31:47 +0200

    Update statusbar.class.js

--- models/statusbar.class.js
+++ models/statusbar.class.js
@@ -1,3 +1,11 @@
 class Statusbar {
-    
+
+    IMAGES_STATUSBAR = [
+        '../img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png',
+        '../img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png',
+        '../img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png',
+        '../img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png',
+        '../img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png',
+        '../img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png'
+    ];
 }
