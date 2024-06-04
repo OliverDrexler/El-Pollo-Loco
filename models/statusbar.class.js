c6885098ec965cd52f141191c739a41312bb64de
Oliver Drexler <oliverdrexler@web.de>
2024-06-04 17:16:29 +0200

    Update statusbar.class.js

--- models/statusbar.class.js
+++ models/statusbar.class.js
@@ -41,7 +41,7 @@ class Statusbar extends DrawableObject {
             return 2;
         } else if (this.percentage > 20) {
             return 1;
-        } else /*if (this.percentage == 0)*/ {
+        } else {
             return 0;
         }
     }
