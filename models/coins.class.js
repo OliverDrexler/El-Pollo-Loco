1385586491faead2c90760a7a7513055262527d1
Oliver Drexler <oliverdrexler@web.de>
2024-06-05 09:50:04 +0200

    added JSDoc

--- models/coins.class.js
+++ models/coins.class.js
@@ -10,17 +10,21 @@ class Coins extends DrawableObject {
     height = 180;
     width = 180;
 
+
+    /**
+     * Creates an instance of Coins.
+     * Loads the initial image and sets up the animation for the coin.
+     */
     constructor() {
         super().loadImage('../../img/8_coin/coin_1.png');
         this.loadImages(this.IMAGES_COINS);
         this.animate();
     }
 
-    /*setImage() {
-        this.img = this.imageCache[this.IMAGES_COINS[this.currentImage]];
-    }*/
 
-
+    /**
+     * This method starts the animation for the coin.
+     */
     animate() {
         setInterval(() => {
             this.playAnimation(this.IMAGES_COINS);
