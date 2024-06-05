5194a1d2c70d7f4a8bc93555ab855283b5f435ac
Oliver Drexler <oliverdrexler@web.de>
2024-06-05 07:55:27 +0200

    ongoing work

--- models/coins.class.js
+++ models/coins.class.js
@@ -5,7 +5,7 @@ class Coins extends MovableObject {
         '../img/8_coin/coin_2.png'
     ];
 
-    x = 100;
+    x = 300;
     y = 100;
     height = 180;
     width = 180;
@@ -15,6 +15,10 @@ class Coins extends MovableObject {
         this.loadImages(this.IMAGES_COINS);
         this.animate();
     }
+
+    /*setImage() {
+        this.img = this.imageCache[this.IMAGES_COINS[this.currentImage]];
+    }*/
 
 
     animate() {
--- models/world.class.js
+++ models/world.class.js
@@ -58,22 +58,19 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        // Verschiebbare Objekte zeichnen
         this.ctx.translate(this.camera_x, 0);
         this.addObjectsToMap(this.level.backgroundObjects);
-
-        
-        this.ctx.translate(-this.camera_x, 0); 
-        // ------- Space for fixed objects ------- //
-        this.addToMap(this.statusbar);
-        this.addToMap(this.coins);
-        this.ctx.translate(this.camera_x, 0); 
-
-        this.addToMap(this.character);
         this.addObjectsToMap(this.level.clouds);
         this.addObjectsToMap(this.level.enemies);
+        this.addToMap(this.character);
 
-        this.ctx.translate(-this.camera_x, 0); // Resets the canvas translation
-        
+        this.ctx.translate(-this.camera_x, 0); // Kamera-Übersetzung zurücksetzen
+
+        // Feste Objekte zeichnen
+        this.addToMap(this.statusbar);
+        this.addToMap(this.coins);
+
         let self = this; // Using 'self' to refer to the current instance in requestAnimationFrame
         requestAnimationFrame(function () {
             self.draw();
