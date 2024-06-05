8f79b1dcc1660eba30a0675bf42d44fed6ea3f20
Oliver Drexler <oliverdrexler@web.de>
2024-06-05 12:01:39 +0200

    added bottles.class.js

--- models/bottles.class.js
+++ models/bottles.class.js
@@ -1,17 +1,19 @@
-class Coins extends DrawableObject {
+class Bottles extends DrawableObject {
 
     IMAGES_COINS = [
         '../img/8_coin/coin_1.png',
         '../img/8_coin/coin_2.png'
     ];
 
+    //x = 300;
+    //y = 100;
     height = 180;
     width = 180;
 
 
     /**
-     * Creates an instance of Coins.
-     * Loads the initial image and sets up the animation for the coin.
+     * Creates an instance of Bottles.
+     * Loads the initial image and sets up the animation for the bottle.
      */
     constructor() {
         super().loadImage('../../img/8_coin/coin_1.png');
