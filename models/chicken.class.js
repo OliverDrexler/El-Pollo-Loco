d214ffda8f6553bb32646352960ef71e1a1cd0a9
Oliver Drexler <oliverdrexler@web.de>
2024-06-05 15:05:12 +0200

    Update chicken.class.js

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -23,8 +23,8 @@ class Chicken extends MovableObject {
     constructor() {
         super().loadImage('../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png'); // Calls the function of the parent class (MovableObject)
         this.loadImages(this.IMAGES_WALKING);
-        this.x = 400 + Math.random() * 2300; // Sets the initial x position to a random value between 200 and 2500
-        this.speed = 0.15 + Math.random() * 0.25; // Sets the speed to a random value between 0.15 and 0.40
+        this.x = 400 + Math.random() * 2300;
+        this.speed = 0.15 + Math.random() * 0.25; 
         this.animate();
     }
 
