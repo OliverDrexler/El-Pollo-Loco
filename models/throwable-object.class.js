fc1503c01268a12bb171966e938283be9fa00c0b
Oliver Drexler <oliverdrexler@web.de>
2024-06-06 18:00:19 +0200

    Update throwable-object.class.js

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -3,6 +3,13 @@ class ThrowableObject extends MovableObject {
     height = 90;
     width = 90;
 
+    IMAGES_BOTTLE = [
+        '../img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        '../img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        '../img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        '../img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
+    ];
+
 
     /**
     * Creates an instance of a ThrowableObject.
@@ -12,7 +19,8 @@ class ThrowableObject extends MovableObject {
     * @param {number} y - The initial y position of the object.
     */
     constructor(x, y) {
-        super().loadImage('../img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png');
+        super().loadImage('../img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png');
+        this.loadImages(this.IMAGES_BOTTLE);
         this.x = x;
         this.y = y;
         this.throw();
@@ -28,7 +36,18 @@ class ThrowableObject extends MovableObject {
         this.speed_y = 28;
         this.applyGravity();
         setInterval(() => {
+            this.animateBottle();
+        }, 80);
+        setInterval(() => {
             this.x += 8;
         }, 25);
+    }
+
+
+    /**
+    * This method plays the bottle animation.
+    */
+    animateBottle() {
+        this.playAnimation(this.IMAGES_BOTTLE);
     }
 }
