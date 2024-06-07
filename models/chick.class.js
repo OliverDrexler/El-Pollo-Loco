f0dc1287c9d9c59b975216ec479d367f682b6903
Oliver Drexler <oliverdrexler@web.de>
2024-06-07 13:22:49 +0200

    Update chick.class.js

--- models/chick.class.js
+++ models/chick.class.js
@@ -1,8 +1,8 @@
 class Chick extends MovableObject {
 
-    y = 365;
-    width = 60;
-    height = 60;
+    y = 355;
+    width = 70;
+    height = 70;
 
     IMAGES_WALKING = [
         '../img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
