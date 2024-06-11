184edeee110389cb2de1156c5ab57a66407cf652
Oliver Drexler <oliverdrexler@web.de>
2024-06-11 07:49:44 +0200

    added more enemies to map

--- models/chick.class.js
+++ models/chick.class.js
@@ -20,11 +20,12 @@ class Chick extends MovableObject {
      * Creates an instance of Chick.
      * Loads the initial image, loads the walking animation images, 
      * sets a random initial x position and speed and starts the animation.
+     * @param {number} [x] - The optional initial x position for the chicken.
      */
-    constructor() {
+    constructor(x) {
         super().loadImage('../img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
-        this.x = 400 + Math.random() * 3700;
+        this.x = x !== undefined ? x : 400 + Math.random() * 3200;
         this.speed = 0.10 + Math.random() * 0.20;
         this.animate();
     }
