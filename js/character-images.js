5378cd7cf4ee51250710bd9a9f040ba6dcbd500f
Oliver Drexler <oliverdrexler@web.de>
2024-07-05 14:05:35 +0200

    Update character-images.js

--- js/character-images.js
+++ js/character-images.js
@@ -1,61 +1,61 @@
 const CHARACTER_IMAGES = {
     IMAGES_IDLE: [
-        '../img/2_character_pepe/1_idle/idle/I-1.png',
-        '../img/2_character_pepe/1_idle/idle/I-2.png',
-        '../img/2_character_pepe/1_idle/idle/I-3.png',
-        '../img/2_character_pepe/1_idle/idle/I-4.png',
-        '../img/2_character_pepe/1_idle/idle/I-5.png',
-        '../img/2_character_pepe/1_idle/idle/I-6.png',
-        '../img/2_character_pepe/1_idle/idle/I-7.png',
-        '../img/2_character_pepe/1_idle/idle/I-8.png',
-        '../img/2_character_pepe/1_idle/idle/I-9.png',
-        '../img/2_character_pepe/1_idle/idle/I-10.png'
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/idle/I-1.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/idle/I-2.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/idle/I-3.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/idle/I-4.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/idle/I-5.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/idle/I-6.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/idle/I-7.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/idle/I-8.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/idle/I-9.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/idle/I-10.png'
     ],
 
     IMAGES_SLEEP: [
-        '../img/2_character_pepe/1_idle/long_idle/I-11.png',
-        '../img/2_character_pepe/1_idle/long_idle/I-12.png',
-        '../img/2_character_pepe/1_idle/long_idle/I-13.png',
-        '../img/2_character_pepe/1_idle/long_idle/I-14.png',
-        '../img/2_character_pepe/1_idle/long_idle/I-15.png',
-        '../img/2_character_pepe/1_idle/long_idle/I-16.png',
-        '../img/2_character_pepe/1_idle/long_idle/I-17.png',
-        '../img/2_character_pepe/1_idle/long_idle/I-18.png',
-        '../img/2_character_pepe/1_idle/long_idle/I-19.png',
-        '../img/2_character_pepe/1_idle/long_idle/I-20.png'
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/long_idle/I-11.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/long_idle/I-12.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/long_idle/I-13.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/long_idle/I-14.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/long_idle/I-15.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/long_idle/I-16.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/long_idle/I-17.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/long_idle/I-18.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/long_idle/I-19.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/1_idle/long_idle/I-20.png'
     ],
 
     IMAGES_WALKING: [
-        '../img/2_character_pepe/2_walk/W-21.png',
-        '../img/2_character_pepe/2_walk/W-22.png',
-        '../img/2_character_pepe/2_walk/W-23.png',
-        '../img/2_character_pepe/2_walk/W-24.png',
-        '../img/2_character_pepe/2_walk/W-25.png',
-        '../img/2_character_pepe/2_walk/W-26.png'
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/2_walk/W-21.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/2_walk/W-22.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/2_walk/W-23.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/2_walk/W-24.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/2_walk/W-25.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/2_walk/W-26.png'
     ],
 
     IMAGES_JUMPING: [
-        '../img/2_character_pepe/3_jump/J-33.png',
-        '../img/2_character_pepe/3_jump/J-34.png',
-        '../img/2_character_pepe/3_jump/J-35.png',
-        '../img/2_character_pepe/3_jump/J-36.png',
-        '../img/2_character_pepe/3_jump/J-37.png',
-        '../img/2_character_pepe/3_jump/J-38.png'
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/3_jump/J-33.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/3_jump/J-34.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/3_jump/J-35.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/3_jump/J-36.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/3_jump/J-37.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/3_jump/J-38.png'
     ],
 
     IMAGES_DEAD: [
-        '../img/2_character_pepe/5_dead/D-51.png',
-        '../img/2_character_pepe/5_dead/D-52.png',
-        '../img/2_character_pepe/5_dead/D-53.png',
-        '../img/2_character_pepe/5_dead/D-54.png',
-        '../img/2_character_pepe/5_dead/D-55.png',
-        '../img/2_character_pepe/5_dead/D-56.png',
-        '../img/2_character_pepe/5_dead/D-57.png'
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/5_dead/D-51.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/5_dead/D-52.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/5_dead/D-53.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/5_dead/D-54.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/5_dead/D-55.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/5_dead/D-56.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/5_dead/D-57.png'
     ],
 
     IMAGES_HURT: [
-        '../img/2_character_pepe/4_hurt/H-41.png',
-        '../img/2_character_pepe/4_hurt/H-42.png',
-        '../img/2_character_pepe/4_hurt/H-43.png'
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/4_hurt/H-41.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/4_hurt/H-42.png',
+        'https://oliverdrexler.com/pollo-loco/img/2_character_pepe/4_hurt/H-43.png'
     ]
-}
+};
